de8f392a148f82ec7074441c63f2fd0e91a872fc
shivero <sh1veeer@gmail.com>
2023-07-27 00:22:58 +0200

    added switchcomponent

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'preact/compat';
+import { useState } from 'preact/hooks';
 
 type CardProps = {
     title: string;
@@ -6,16 +6,18 @@ type CardProps = {
 };
 
 export const Card = ({ title, description }: CardProps) => {
-    const [count, setCount] = useState(0)
 
     return (
         <div className="bg-slate-100 shadow-lg overflow-hidden fixed bottom-0 left-0 right-0 ">
-            {/* {count} */}
-            {/* <button onClick={() => setCount(count + 1)}>+</button> */}
             <div className="container mx-auto px-6 py-4">
                 <h2 className="text-2xl mb-2">{title}</h2>
                 <div className="grid gap-8 grid-cols-[2fr_2fr_1fr]">
                     <div>
+                        {/* tailwindcss switch like checkbox */}
+                        <div className="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                            <SwitchCheckbox />
+                        </div>
+
                         <div className="text-gray-700 text-sm">
                             <p className="pb-4">Dzięki plikom cookies i technologiom pokrewnym oraz przetwarzaniu Twoich danych, możemy zapewnić, że dopasujemy do Ciebie wyświetlane treści.</p>
                             <p className="pb-4">Wyrażając zgodę na przechowywanie informacji na urządzeniu końcowym lub dostęp do nich i przetwarzanie danych (w tym w obszarze profilowania, analiz rynkowych i statystycznych) sprawiasz, że łatwiej będzie odnaleźć Ci w Allegro dokładnie to, czego szukasz i potrzebujesz.</p>
@@ -39,3 +41,26 @@ export const Card = ({ title, description }: CardProps) => {
         </div>
     );
 };
+
+
+const SwitchCheckbox = () => {
+    const [isToggled, setIsToggled] = useState(false)
+    if (isToggled) {
+        return (
+            <>
+                <input type="checkbox" onClick={() => setIsToggled(!isToggled)} name="toggle" id="toggle" className="toggle-checkbox right-0 absolute  block w-6 h-6 rounded-full transition-all bg-white border-4 appearance-none cursor-pointer" checked />
+                <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-blue-400 cursor-pointer"></label>
+            </>
+
+        )
+    }
+    else {
+        return (
+            <>
+                <input type="checkbox" onClick={() => setIsToggled(!isToggled)} name="toggle" id="toggle" className="toggle-checkbox right-4 absolute block transition-all w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer" />
+                <label htmlFor="toggle" className="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+            </>
+        )
+    }
+
+}
